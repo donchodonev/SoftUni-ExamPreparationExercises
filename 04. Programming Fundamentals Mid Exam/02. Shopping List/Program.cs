a134c3b7549d36317ea70277d404d1b2e7bcf764
donchodonev <doniodonef@gmail.com>
2021-07-05 12:12:05 +0300

    added 2nd exam exercise solution and it's .docx conditions file

---
+++ 04. Programming Fundamentals Mid Exam/02. Shopping List/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine()
+                .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            string input = Console.ReadLine();
+
+            while (input != "Go Shopping!")
+            {
+                string[] cmdArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string command = cmdArgs[0];
+                string item = cmdArgs[1];
+
+                if (command == "Urgent")
+                {
+                    if (!groceries.Contains(item))
+                    {
+                        groceries.Insert(0, item);
+                    }
+                }
+                else if (command == "Unnecessary")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                    }
+                }
+                else if (command == "Correct")
+                {
+                    string newItem = cmdArgs[2];
+
+                    if (groceries.Contains(item))
+                    {
+                        groceries[groceries.IndexOf(item)] = newItem;
+                    }
+                }
+                else if (command == "Rearrange")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                        groceries.Add(item);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ",groceries));
+        }
+    }
+}
