8170c4c8622b5d55ff23cceecee4ab60aee17dd5
donchodonev <dummyacc9292@gmail.com>
2021-07-05 16:00:45 +0300

    added another exam exercise solution added .docx exercise conditions file

---
+++ 02. Programming Fundamentals Mid Exam/02. Array Modifier/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Array_Modifier
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string action = cmdArgs[0];
+
+                if (action == "swap")
+                {
+                    int firstIndex = int.Parse(cmdArgs[1]);
+                    int secondIndex = int.Parse(cmdArgs[2]);
+
+                    int firstIndexNum = numbers[firstIndex];
+                    int secondIndexNum = numbers[secondIndex];
+
+                    numbers[firstIndex] = secondIndexNum;
+                    numbers[secondIndex] = firstIndexNum;
+                }
+                else if (action == "multiply")
+                {
+                    int firstIndex = int.Parse(cmdArgs[1]);
+                    int secondIndex = int.Parse(cmdArgs[2]);
+
+                    numbers[firstIndex] *= numbers[secondIndex];
+                }
+                else if (action == "decrease")
+                {
+                    numbers = numbers.Select(x => x -= 1).ToArray();
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
