5e8b8c4e6e0a776656062c9c209994b6baa582c9
donchodonev <doniodonef@gmail.com>
2021-07-05 11:59:58 +0300

    added first exam task solution and .docx conditions file

---
+++ 04. Programming Fundamentals Mid Exam/01. National Court/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int efficiencyPerHour = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                efficiencyPerHour += int.Parse(Console.ReadLine());
+            }
+
+            int peopleToBeAnswered = int.Parse(Console.ReadLine());
+
+            int hoursNeeded = 0;
+
+
+            while (peopleToBeAnswered > 0)
+            {
+                for (int i = 0; i < 3 && peopleToBeAnswered > 0; i++)
+                {
+                    peopleToBeAnswered -= efficiencyPerHour;
+                    hoursNeeded++;
+
+                    if (i == 2 && peopleToBeAnswered > 0)
+                    {
+                        hoursNeeded++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hoursNeeded}h.");
+        }
+    }
+}
