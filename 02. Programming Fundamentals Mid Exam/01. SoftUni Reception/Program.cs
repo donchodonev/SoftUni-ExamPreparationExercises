b57fd7e11cba66ebe625bc1d359844e4cee81402
donchodonev <dummyacc9292@gmail.com>
2021-07-05 15:42:42 +0300

    added first exam exercise solution and it's .docx conditions file

---
+++ 02. Programming Fundamentals Mid Exam/01. SoftUni Reception/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._SoftUni_Reception
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int efficiencyPerHour = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                efficiencyPerHour += int.Parse(Console.ReadLine());
+            }
+
+            int peopleToBeAnswered = int.Parse(Console.ReadLine());
+
+            int hoursNeeded = 0;
+
+            while (peopleToBeAnswered > 0)
+            {
+                for (int i = 0; i < 3 && peopleToBeAnswered > 0; i++)
+                {
+                    peopleToBeAnswered -= efficiencyPerHour;
+                    hoursNeeded++;
+
+                    if (i == 2 && peopleToBeAnswered > 0)
+                    {
+                        hoursNeeded++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hoursNeeded}h.");
+        }
+    }
+}
