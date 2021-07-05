2cbfd569f90973c71b5d406c4a58523517f51ab2
donchodonev <doniodonef@gmail.com>
2021-07-05 11:29:41 +0300

    added final exam task solution and .docx exam conditions file

---
+++ 05. Programming Fundamentals Mid Exam/03. Inventory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inventory = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Craft!")
+            {
+                string command = input.Split(" - ", StringSplitOptions.RemoveEmptyEntries)[0];
+
+                if (command == "Collect")
+                {
+                    string item = input.Split(" - ", StringSplitOptions.RemoveEmptyEntries)[1];
+
+                    if (!inventory.Contains(item))
+                    {
+                        inventory.Add(item);
+                    }
+                }
+                else if (command == "Drop")
+                {
+                    string item = input.Split(" - ", StringSplitOptions.RemoveEmptyEntries)[1];
+
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                    }
+                }
+                else if (command == "Combine Items")
+                {
+                    string items = input.Split(" - ", StringSplitOptions.RemoveEmptyEntries)[1];
+
+                    string oldItem = items.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+                    string newItem = items.Split(":", StringSplitOptions.RemoveEmptyEntries)[1];
+
+                    if (inventory.Contains(oldItem))
+                    {
+                        inventory.Insert(inventory.IndexOf(oldItem) + 1, newItem);
+                    }
+                }
+                else if (command == "Renew")
+                {
+                    string item = input.Split(" - ", StringSplitOptions.RemoveEmptyEntries)[1];
+
+                    if (inventory.Contains(item))
+                    {
+                        inventory.Remove(item);
+                        inventory.Add(item);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ",inventory));
+
+        }
+    }
+}
