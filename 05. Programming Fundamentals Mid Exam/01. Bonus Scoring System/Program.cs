d7270e68ced5fd1d9a267b8dc9612ad4370147b0
donchodonev <doniodonef@gmail.com>
2021-07-05 11:28:47 +0300

    added first exam task solution and .docx exam conditions file

---
+++ 05. Programming Fundamentals Mid Exam/01. Bonus Scoring System/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double studentCount = int.Parse(Console.ReadLine());
+            double lecturesCount = int.Parse(Console.ReadLine());
+            double additionalBonus = int.Parse(Console.ReadLine());
+
+            if (studentCount == 0)
+            {
+                Console.WriteLine($"Max Bonus: 0.");
+                Console.WriteLine($"The student has attended 0 lectures.");
+                return;
+            }
+
+            List<int> studentsAttendance = new List<int>();
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                studentsAttendance.Add(int.Parse(Console.ReadLine()));
+            }
+
+            double mostAttendingStudent = studentsAttendance.OrderByDescending(x => x).First();
+
+            double currentBonus = Math.Ceiling(mostAttendingStudent / lecturesCount * (5 + additionalBonus));
+
+            Console.WriteLine($"Max Bonus: {currentBonus}.");
+            Console.WriteLine($"The student has attended {studentsAttendance.Max()} lectures.");
+
+        }
+    }
+}
