0b024b8ba8ec3241f46cbc995d2bc18c04101c87
donchodonev <doniodonef@gmail.com>
2021-07-05 14:14:00 +0300

    added first exam exercise solution at 71/100 points added .docx exam exercise conditions

---
+++ 03. Programming Fundamentals Mid Exam Retake/01. Counter Strike/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            int wonBattles = 0;
+
+            while (command != "End of battle")
+            {
+                int leftEnergy = energy - int.Parse(command);
+
+                if (leftEnergy == 0)
+                {
+                    energy -= int.Parse(command);
+                    wonBattles++;
+                    Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+                    return;
+                }
+                else if (leftEnergy < 0)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+                    return;
+                }
+                else
+                {
+                    energy -= int.Parse(command);
+                    wonBattles++;
+                }
+
+                if (wonBattles % 3 == 0)
+                {
+                    energy += wonBattles;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Won battles: {wonBattles}. Energy left: {energy}");
+        }
+    }
+}
