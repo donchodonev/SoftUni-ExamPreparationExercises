0336c0fe4454e08b25a0d4942793eeb716957d26
donchodonev <dummyacc9292@gmail.com>
2021-08-09 12:25:53 +0300

    added another exercise solution

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLocations = Console.ReadLine();
+
+            Regex regex = new Regex(@"([=\/])(?'location'[A-Z][A-Za-z]{2,})\1");
+
+            var matchCollection = regex.Matches(inputLocations);
+
+            List<string> destionations = new List<string>();
+
+            int travelPoints = 0;
+
+            foreach (Match match in matchCollection)
+            {
+                destionations.Add(match.Groups["location"].Value);
+                travelPoints += match.Groups["location"].Value.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ",destionations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
