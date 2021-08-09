b4a911a2955e8b5081f6add813b64bf03175e1e9
donchodonev <dummyacc9292@gmail.com>
2021-08-09 10:01:14 +0300

    added final exam first exercise solution

---
+++ 01. Programming Fundamentals Final Exam Retake/01. The Imitation Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01._The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            string[] cmdArgs = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            string command = cmdArgs[0];
+
+            while (command != "Decode")
+            {
+                if (command == "Move")
+                {
+                    Queue rotatedMessage = new Queue();
+
+                    int charsToRotate = int.Parse(cmdArgs[1]);
+
+                    foreach (var character in encryptedMessage)
+                    {
+                        rotatedMessage.Enqueue(character);
+                    }
+
+                    for (int i = 0; i < charsToRotate; i++)
+                    {
+                        rotatedMessage.Enqueue(rotatedMessage.Dequeue());
+                    }
+
+                    encryptedMessage = string.Empty;
+
+                    foreach (var character in rotatedMessage)
+                    {
+                        encryptedMessage += character;
+                    }
+                }
+                else if (command == "Insert")
+                {
+                    encryptedMessage = encryptedMessage.Insert(int.Parse(cmdArgs[1]), cmdArgs[2]);
+                }
+                else if (command == "ChangeAll")
+                {
+                    encryptedMessage = encryptedMessage.Replace(cmdArgs[1], cmdArgs[2]);
+                }
+
+                cmdArgs = Console.ReadLine()
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                command = cmdArgs[0];
+            }
+
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+        }
+    }
+}
