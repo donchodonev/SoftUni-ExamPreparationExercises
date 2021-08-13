3ebe37731e90a27d52043ec7fc15282478c91482
donchodonev <dummyacc9292@gmail.com>
2021-08-13 11:17:32 +0300

    added another exercise solution added exercise .docx problem description file

---
+++ 04. Programming Fundamentals Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+");
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> barcodes = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                barcodes.Add(Console.ReadLine());
+            }
+
+            foreach (var barcode in barcodes)
+            {
+                if (!regex.IsMatch(barcode))
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                else
+                {
+                    string defaultGroup = "00";
+                    string productGroup = String.Empty;
+
+                    foreach (var character in barcode)
+                    {
+                        if (char.IsDigit(character))
+                        {
+                            productGroup += character;
+                        }
+                    }
+
+                    if (productGroup != "")
+                    {
+                        Console.WriteLine($"Product group: {productGroup}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {defaultGroup}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04. Programming Fundamentals Final Exam/03. Heroes of Code and Logic VII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Heroes_of_Code_and_Logic_VII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
