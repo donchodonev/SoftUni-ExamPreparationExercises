aec39eeb8b0dcdcb7333505c65e079e5e3315c3a
donchodonev <dummyacc9292@gmail.com>
2021-10-20 14:17:55 +0300

    added another exercise solution

---
+++ C# Advanced Exam - 26 June 2021/ExamPreparation/SkiRental/SkiRental.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        public SkiRental(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Ski>();
+        }
+
+        private List<Ski> data { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Ski ski)
+        {
+            if (Capacity > Count)
+            {
+                data.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Ski ski = GetSki(manufacturer,model);
+
+            if (ski == null)
+            {
+                return false;
+            }
+            else
+            {
+                data.Remove(ski);
+                return true;
+            }
+        }
+
+        public Ski GetNewestSki()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The skis stored in {Name}:");
+
+            foreach (var ski in data)
+            {
+                sb.AppendLine(ski.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
