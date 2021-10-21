742c480990499319af32a4d47d7886edbed7a742
donchodonev <dummyacc9292@gmail.com>
2021-10-21 14:49:05 +0300

    added final exercise solution

---
+++ C# Advanced Retake Exam - 14 April 2021/Exercises/Cocktail Party/Cocktail.cs
@@ -0,0 +1,77 @@
+﻿namespace CocktailParty
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            Ingredients = new List<Ingredient>();
+        }
+
+        public List<Ingredient> Ingredients { get; set; }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return Ingredients.FirstOrDefault(x => x.Name == name);
+        }
+        public int CurrentAlcoholLevel => Ingredients.Sum(x => x.Alcohol);
+
+        public void Add(Ingredient ingredient)
+        {
+            if (Ingredients.All(x => x.Name != ingredient.Name))
+            {
+                if (Capacity > Ingredients.Count)
+                {
+                    if (CurrentAlcoholLevel + ingredient.Alcohol <= MaxAlcoholLevel)
+                    {
+                        Ingredients.Add(ingredient);
+                    }
+                }
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient ingredient = FindIngredient(name);
+
+            if (ingredient == null)
+            {
+                return false;
+            }
+
+            Ingredients.Remove(ingredient);
+
+            return true;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var ingredient in Ingredients)
+            {
+                sb.AppendLine(ingredient.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced Retake Exam - 14 April 2021/Exercises/Warm Winter/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Warm_Winter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            List<int> sets = new List<int>();
+
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                if (hat > scarf)
+                {
+                    sets.Add(hat + scarf);
+                    scarfs.Dequeue();
+                    hats.Pop();
+                    continue;
+                }
+                else if (hat == scarf)
+                {
+                    scarfs.Dequeue();
+                    hats.Pop();
+                    hats.Push(++hat);
+                    continue;
+                }
+
+                while (hats.Count > 0)
+                {
+                    if (hats.Peek() < scarf)
+                    {
+                        hats.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.OrderByDescending(x => x).First()}");
+            Console.WriteLine(string.Join(' ', sets));
+        }
+    }
+}
